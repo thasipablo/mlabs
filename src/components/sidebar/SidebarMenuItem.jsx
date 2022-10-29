56adb63632a0edea6e9e37497086154ebb8a6e5f
thasipablo <thasipablo@gmail.com>
2022-10-29 16:36:31 +0100

    add menu icons

--- src/components/sidebar/SidebarMenuItem.jsx
+++ src/components/sidebar/SidebarMenuItem.jsx
@@ -5,7 +5,10 @@ const SidebarMenuItem = ({ icon, text, link }) => {
   return (
     <NavLink to={link} className="sidebar-menu-item">
       {/* <img className="sidebar-menu-icon" src={icon} alt="menu icon" /> */}
-      <div>{text}</div>
+      <div className="content">
+        <span className="icon">{icon}</span>
+        {text}
+      </div>
     </NavLink>
   );
 };
